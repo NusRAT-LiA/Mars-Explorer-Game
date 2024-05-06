5c0693578b1533c7fe0c9d3409604d705b40812c
Hack-it-Rafi <zozorafi2016@gmail.com>
2024-05-06 22:41:17 +0600

    Shovel changed in level3

--- Assets/Scripts/WeatherClient.cs
+++ Assets/Scripts/WeatherClient.cs
@@ -9,8 +9,7 @@ using System;
 public class WeatherClient : MonoBehaviour
 {
     private RequestSocket clientSocket;
-    public TMPro.TextMeshProUGUI predictionText; // Reference to your UI Text element
-
+    public TMPro.TextMeshProUGUI predictionText;
     async void Start()
     {
         ForceDotNet.Force();
@@ -19,13 +18,11 @@ public class WeatherClient : MonoBehaviour
         clientSocket = new RequestSocket();
         clientSocket.Connect("tcp://localhost:5555");
 
-        // Start receiving predictions from the server
         await StartReceivingPredictions();
     }
 
     void OnDestroy()
     {
-        // Clean up resources
         clientSocket.Close();
         NetMQConfig.Cleanup();
     }
@@ -44,14 +41,12 @@ public class WeatherClient : MonoBehaviour
             // Update UI with the received prediction data
             UpdatePredictionUI(data);
 
-            // Wait for a while before sending the next request
-            await Task.Delay(5000); // Adjust the delay time as needed
+            await Task.Delay(5000);
         }
     }
 
     void UpdatePredictionUI(Data data)
     {
-        // Truncate uv_index_encoded to its integer part
         int truncatedUVIndex = Mathf.FloorToInt(data.uv_index_encoded);
         string uvIndex;
 
@@ -74,11 +69,9 @@ public class WeatherClient : MonoBehaviour
                 break;
         }
 
-        // Define units for each data field
         string TempUnit = "°C";
         string pressureUnit = "Pa";
         DateTime today = DateTime.Today;
-        // Update UI with the received prediction data and units
         predictionText.text = $"Date:" + today.ToString("yyyy-MM-dd") + "\n\n" +
                               $"Min Temp: {data.min_temp:F2}{TempUnit}\n" +
                               $"Max Temp: {data.max_temp:F2}{TempUnit}\n" +
